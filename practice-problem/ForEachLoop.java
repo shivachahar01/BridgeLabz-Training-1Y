d52037f27fe5e397de1e52dd4bea270751811a37
Shiva chahar <shivachahar0001@gmail.com>
2026-01-28 15:14:48 +0530

    loops

---
+++ practice-problem/ForEachLoop.java
@@ -0,0 +1,10 @@
+package day1.src.ClassWork;
+
+public class ForEachLoop {
+    public static void main(String[] args){
+        int arr[] = {10, 20 , 30 , 40 , 50 };
+        for(int x : arr){
+            System.out.println(x);
+        }
+    }
+}
